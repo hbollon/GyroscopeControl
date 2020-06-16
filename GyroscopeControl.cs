20f991ea668b70558539bcff543b2f6eab8db277
Hugo <hugo.bollon@gmail.com>
2020-06-16 16:54:26 +0200

    Check if device support gyro & increase update interval on init

--- GyroscopeControl.cs
+++ GyroscopeControl.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GyroscopeControl : MonoBehaviour
 {
@@ -10,23 +8,30 @@ public class GyroscopeControl : MonoBehaviour
     private Quaternion gyroInitialRotation;
 
     public bool GyroEnabled { get; set; }
+    private bool gyroInitialized = false;
 
     // SETTINGS
     [SerializeField] private float smoothing = 0.1f;
     [SerializeField] private float speed = 60.0f;
 
-    private void Awake() {
-        Input.gyro.enabled = true;
+    private void InitGyro() {
+        if(!gyroInitialized){
+            Input.gyro.enabled = true;
+            Input.gyro.updateInterval = 0.0167f;
+        }
+        gyroInitialized = true;
     }
 
     private void Start()
     {
-        Application.targetFrameRate = 60;
-        GyroEnabled = true;
+        if(HasGyro()){
+            InitGyro();
+            GyroEnabled = true;
+        } else GyroEnabled = false;
         
         /* Get object and gyroscope initial rotation for calibration */
         initialRotation = transform.rotation; 
-        recalibrate();
+        Recalibrate();
 
         /* GameObject instance used to prepare object movement */
         rawGyroRotation = new GameObject("GyroRaw").transform;
@@ -52,8 +57,13 @@ public class GyroscopeControl : MonoBehaviour
             -Input.gyro.attitude.x * curSpeed, 
             0.0f, 
             -Input.gyro.attitude.y * curSpeed, 
-            Input.gyro.attitude.w * curSpeed);
+            Input.gyro.attitude.w * curSpeed
+        );
         rawGyroRotation.rotation = tempGyroRotation;
+    }
+
+    private bool HasGyro(){
+        return SystemInfo.supportsGyroscope;
     }
 
     public void setSpeed(float speed){
@@ -65,7 +75,7 @@ public class GyroscopeControl : MonoBehaviour
     } 
 
     /* Used for calibrate gyro at start or during execution using UI button for exemple */
-    public void recalibrate(){
+    public void Recalibrate(){
         gyroInitialRotation.x = -Input.gyro.attitude.x;
         gyroInitialRotation.y = 0.0f; // Fixed Y axis
         gyroInitialRotation.z = -Input.gyro.attitude.y; // We rotate object on Y with Z axis gyro
